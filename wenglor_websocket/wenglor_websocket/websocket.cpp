70d82ef7421560569bee39a0a31a274fbf3932dd
Chirtop <chircatalinionut2@gmail.com>
2023-11-24 17:53:15 +0200

    Rename all variables to snake case

--- wenglor_websocket/wenglor_websocket/websocket.cpp
+++ wenglor_websocket/wenglor_websocket/websocket.cpp
@@ -20,39 +20,39 @@
 const unsigned short int PORT = 8080;
 const PCWSTR ADDRESS = L"127.0.0.1";
 
-std::map<std::string_view, std::string> extensionMap;
+std::map<std::string_view, std::string> extension_map;
 enum struct ErrorCodes { SOCKET_INITIALIZE_ERROR_CODE = 1,
 						 SOCKET_BIND_ERROR_CODE,
 						 SOCKET_LISTEN_ERROR_CODE};
 
 // Map of file extensions and their content types
 std::map<std::string_view, std::string> CreateExtensionMap() {
-		std::map<std::string_view, std::string> extensionMap;
+		std::map<std::string_view, std::string> extension_map;
 
-		extensionMap["html"] = "text/html";
-		extensionMap["css"] = "text/css";
-		extensionMap["js"] = "application/javascript";
-		extensionMap["jpg"] = "image/jpeg";
-		extensionMap["jpeg"] = "image/jpeg";
-		extensionMap["png"] = "image/png";
-		extensionMap["gif"] = "image/gif";
-		extensionMap["ico"] = "image/x-icon";
-		extensionMap["svg"] = "image/svg+xml";
+		extension_map["html"] = "text/html";
+		extension_map["css"] = "text/css";
+		extension_map["js"] = "application/javascript";
+		extension_map["jpg"] = "image/jpeg";
+		extension_map["jpeg"] = "image/jpeg";
+		extension_map["png"] = "image/png";
+		extension_map["gif"] = "image/gif";
+		extension_map["ico"] = "image/x-icon";
+		extension_map["svg"] = "image/svg+xml";
 
-		return extensionMap;
+		return extension_map;
 }
 
 
 
 // Function that gets the extension of requested file and returns the content type to be put in the http header
 // c++ 17 feature used: string_view - more memory-efficient because the value of the string does not get modified
-std::string GetContentTypeFromExtension(const std::string_view& filePath){
-	std::any dotPos = filePath.find_last_of(".");
-	if (std::any_cast<size_t>(dotPos) != std::string::npos) {
-		std::string_view extension = filePath.substr(std::any_cast<size_t>(dotPos) + 1);
+std::string GetContentTypeFromExtension(const std::string_view& file_path){
+	std::any dot_pos = file_path.find_last_of(".");
+	if (std::any_cast<size_t>(dot_pos) != std::string::npos) {
+		std::string_view extension = file_path.substr(std::any_cast<size_t>(dot_pos) + 1);
 
 		//use of structured bindings
-		for (const auto& [key, value] : extensionMap) {
+		for (const auto& [key, value] : extension_map) {
 			if (key == extension) {
 				return value;
 			}
@@ -62,89 +62,86 @@ std::string GetContentTypeFromExtension(const std::string_view& filePath){
 }
 
 // Returns current time in HTTP format
-std::string getCurrentTimeHttpFormat() {
+std::string GetCurrentTimeHttpFormat() {
 	std::timespec ts;
 	if (std::timespec_get(&ts, TIME_UTC) != 0) {
-		std::tm gmTime;
-		gmtime_s(&gmTime, &ts.tv_sec);
+		std::tm gm_time;
+		gmtime_s(&gm_time, &ts.tv_sec);
 
 		std::ostringstream oss;
-		oss << std::put_time(&gmTime, "Date: %a, %d %b %Y %H:%M:%S GMT\r\n");
+		oss << std::put_time(&gm_time, "Date: %a, %d %b %Y %H:%M:%S GMT\r\n");
 		return oss.str();
 	}
 	return "Failed to get current time.";
 }
 
-void sendNotFoundError(SOCKET clientSocket) {
-	std::string notFoundContent = "<html><head><title>404 Not Found</title></head><body><h1>404 Not Found</h1></body></html>";
-	std::string httpResponseHeader = "HTTP/1.1 404 Not Found\r\n" + getCurrentTimeHttpFormat() + "Content-Type: text/html\r\nContent-Length: " + std::to_string(notFoundContent.length()) + "\r\n\r\n";
-	send(clientSocket, httpResponseHeader.c_str(), httpResponseHeader.length(), 0);
-	send(clientSocket, notFoundContent.c_str(), notFoundContent.length(), 0);
+void SendNotFoundError(SOCKET client_socket) {
+	std::string not_found_content = "<html><head><title>404 Not Found</title></head><body><h1>404 Not Found</h1></body></html>";
+	std::string http_response_header = "HTTP/1.1 404 Not Found\r\n" + GetCurrentTimeHttpFormat() + "Content-Type: text/html\r\nContent-Length: " + std::to_string(not_found_content.length()) + "\r\n\r\n";
+	send(client_socket, http_response_header.c_str(), http_response_header.length(), 0);
+	send(client_socket, not_found_content.c_str(), not_found_content.length(), 0);
 }
 
 // Sends resource from path given in the http request header
-void sendResource(SOCKET clientSocket, const std::string& requestedResourcePath) {
+void SendResource(SOCKET client_socket, const std::string& requested_resource_path) {
 	std::ifstream t;
-	std::filesystem::path projectPath = std::filesystem::current_path();
-	std::string fullPath = projectPath.string() + "/public" + requestedResourcePath;
-	std::stringstream resourceContent;
-	std::string httpResponseHeader = "";
+	std::filesystem::path project_path = std::filesystem::current_path();
+	std::string full_path = project_path.string() + "/public" + requested_resource_path;
+	std::stringstream resource_content;
+	std::string http_response_header = "";
 
 	// if resource is not found - return 404
-	if (!std::filesystem::exists(fullPath)) {
-		sendNotFoundError(clientSocket);
-		std::cout << requestedResourcePath << " not found" << std::endl;
+	if (!std::filesystem::exists(full_path)) {
+		SendNotFoundError(client_socket);
+		std::cout << requested_resource_path << " not found" << std::endl;
 		return;
 	}
 
-	std::string contentType = GetContentTypeFromExtension(requestedResourcePath);
-	t.open(fullPath);
-	resourceContent << t.rdbuf();
+	std::string content_type = GetContentTypeFromExtension(requested_resource_path);
+	t.open(full_path);
+	resource_content << t.rdbuf();
 
-	httpResponseHeader = "HTTP/1.1 200 OK\r\n" + getCurrentTimeHttpFormat() + "Content-Type: " + contentType + "\r\nContent-Length: " + std::to_string(resourceContent.str().length()) + "\r\n\r\n";
+	http_response_header = "HTTP/1.1 200 OK\r\n" + GetCurrentTimeHttpFormat() + "Content-Type: " + content_type + "\r\nContent-Length: " + std::to_string(resource_content.str().length()) + "\r\n\r\n";
 
-	send(clientSocket, httpResponseHeader.c_str(), httpResponseHeader.length(), 0);
-	send(clientSocket, resourceContent.str().c_str(), resourceContent.str().length(), 0);
-	std::cout << requestedResourcePath << " sent\n\n";
+	send(client_socket, http_response_header.c_str(), http_response_header.length(), 0);
+	send(client_socket, resource_content.str().c_str(), resource_content.str().length(), 0);
+	std::cout << requested_resource_path << " sent\n\n";
 }
 
 SOCKET InitializeSocket() {
-	WORD wVersionRequested = MAKEWORD(2, 2);
+	WORD w_version_requested = MAKEWORD(2, 2);
 	WSADATA data;
-	int status = WSAStartup(wVersionRequested, &data);
+	int status = WSAStartup(w_version_requested, &data);
 
 	if (status != 0) {
 		std::cout << "winsock dll not found" << std::endl;
 		return INVALID_SOCKET;
 	}
 
-	std::cout << "winsock found" << std::endl;
-	std::cout << "STATUS: " << data.szSystemStatus << std::endl;
+	SOCKET my_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-	SOCKET mySocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
-	if (mySocket == INVALID_SOCKET) {
+	if (my_socket == INVALID_SOCKET) {
 		std::cout << "\n\nsocket initialization failed with error code: " << WSAGetLastError << std::endl;
 		WSACleanup();
 		return INVALID_SOCKET;
 	}
 
-	std::cout << "\n\nsocket intialized successfully!" << std::endl;
+	std::cout << "socket intialized successfully!" << std::endl;
 
-	return mySocket;
+	return my_socket;
 }
 //std::as_const()
-bool BindSocket(SOCKET mySocket) {
-	sockaddr_in bindInformation;
-	bindInformation.sin_port = htons(std::as_const(PORT));
-	bindInformation.sin_family = AF_INET;
-	InetPton(AF_INET, std::as_const(ADDRESS), &bindInformation.sin_addr.S_un.S_addr);
+bool BindSocket(SOCKET my_socket) {
+	sockaddr_in bind_information;
+	bind_information.sin_port = htons(std::as_const(PORT));
+	bind_information.sin_family = AF_INET;
+	InetPton(AF_INET, std::as_const(ADDRESS), &bind_information.sin_addr.S_un.S_addr);
 
-	int bindStatus = bind(mySocket, (sockaddr*)&bindInformation, sizeof(bindInformation));
+	int bind_status = bind(my_socket, (sockaddr*)&bind_information, sizeof(bind_information));
 
-	if (bindStatus == SOCKET_ERROR) {
+	if (bind_status == SOCKET_ERROR) {
 		std::cout << "socket binding failed with error code: " << WSAGetLastError() << std::endl;
-		closesocket(mySocket);
+		closesocket(my_socket);
 		WSACleanup();
 		return false;
 	}
@@ -153,8 +150,8 @@ bool BindSocket(SOCKET mySocket) {
 	return true;
 }
 
-bool PlaceSocketInListeningMode(SOCKET mySocket) {
-	if (listen(mySocket, 1) != 0) {
+bool PlaceSocketInListeningMode(SOCKET my_socket) {
+	if (listen(my_socket, 1) != 0) {
 		std::cout << "socket can't be put in listening mode " << WSAGetLastError() << std::endl;
 		return false;
 	}
@@ -164,69 +161,69 @@ bool PlaceSocketInListeningMode(SOCKET mySocket) {
 }
 
 // Takes all data received in the request and returns the requested resource
-std::string getRequestedResourceFromRequest(char dataReceivedFromRequest[]) {
-	std::string requestedResource = "";
-	int resourcePathOffset = 4;
+std::string GetRequestedResourceFromRequest(char data_received_from_request[]) {
+	std::string requested_resource = "";
+	int resource_path_offset = 4;
 
-	char* startOfRequestLine = strstr(dataReceivedFromRequest, "GET ");
-	if (startOfRequestLine != nullptr) {
-		char* endOfRequestLine = strstr(startOfRequestLine, " HTTP/1.1");
-		if (endOfRequestLine != nullptr) {
-			requestedResource.assign(startOfRequestLine + resourcePathOffset, endOfRequestLine);
-			std::cout << "Requested Resource: " << requestedResource << std::endl;
+	char* start_of_request_line = strstr(data_received_from_request, "GET ");
+	if (start_of_request_line != nullptr) {
+		char* end_of_requestLine = strstr(start_of_request_line, " HTTP/1.1");
+		if (end_of_requestLine != nullptr) {
+			requested_resource.assign(start_of_request_line + resource_path_offset, end_of_requestLine);
+			std::cout << "Requested Resource: " << requested_resource << std::endl;
 		}
 	}
-	return requestedResource;
+	return requested_resource;
 }
 
 // Accepts connection by creating an accept socket from the listening socket
-SOCKET AcceptConnection(SOCKET listeningSocket) {
-	SOCKET acceptSocket = accept(listeningSocket, NULL, NULL);
-	if (acceptSocket == INVALID_SOCKET) {
+SOCKET AcceptConnection(SOCKET listening_socket) {
+	SOCKET accept_socket = accept(listening_socket, NULL, NULL);
+	if (accept_socket == INVALID_SOCKET) {
 		std::cout << "couldn't accept connection! error code: " << WSAGetLastError() << std::endl;
 		WSACleanup();
 	}
 	else {
 		std::cout << "connected" << std::endl;
 	}
-	return acceptSocket;
+	return accept_socket;
 }
 
 int main()
 {
-	SOCKET mySocket = InitializeSocket();
-	if(mySocket == INVALID_SOCKET) return static_cast<int>(ErrorCodes::SOCKET_INITIALIZE_ERROR_CODE);
-	if (BindSocket(mySocket) == 0) return static_cast<int>(ErrorCodes::SOCKET_BIND_ERROR_CODE);
-	if (PlaceSocketInListeningMode(mySocket) == 0) return static_cast<int>(ErrorCodes::SOCKET_LISTEN_ERROR_CODE);
+	SOCKET my_socket = InitializeSocket();
+	if(my_socket == INVALID_SOCKET) return static_cast<int>(ErrorCodes::SOCKET_INITIALIZE_ERROR_CODE);
+	if (BindSocket(my_socket) == 0) return static_cast<int>(ErrorCodes::SOCKET_BIND_ERROR_CODE);
+	if (PlaceSocketInListeningMode(my_socket) == 0) return static_cast<int>(ErrorCodes::SOCKET_LISTEN_ERROR_CODE);
 
-	extensionMap = CreateExtensionMap();
+	extension_map = CreateExtensionMap();
 
 	while (true) {
-		SOCKET acceptSocket = AcceptConnection(mySocket);
+		SOCKET accept_socket = AcceptConnection(my_socket);
 
 		while (true) {
-			char dataReceivedFromRequest[4096];
-			int numberOfReceivedBytes = recv(acceptSocket, dataReceivedFromRequest, 4096, 0);
-			if (numberOfReceivedBytes == SOCKET_ERROR) {
+			char data_received_from_request[4096];
+			int number_of_received_bytes = recv(accept_socket, data_received_from_request, 4096, 0);
+			if (number_of_received_bytes == SOCKET_ERROR) {
 				std::cout << "couldn't receive message: " << WSAGetLastError() << std::endl;
 				break;
 			}
 			else {
-				std::cout << "received " << numberOfReceivedBytes << " bytes" << std::endl;
+				std::cout << "received " << number_of_received_bytes << " bytes" << std::endl;
 			}
-			std::string requestedResourcePath = getRequestedResourceFromRequest(dataReceivedFromRequest);
+			std::string requested_resource_path = GetRequestedResourceFromRequest(data_received_from_request);
 			
-			sendResource(acceptSocket, (requestedResourcePath == "/") ? "/index.html" : requestedResourcePath);
+			SendResource(accept_socket, (requested_resource_path == "/") ? "/index.html" : requested_resource_path);
 
-			if (strstr(dataReceivedFromRequest, "Connection: keep-alive") == nullptr || numberOfReceivedBytes == 0) {
+			if (strstr(data_received_from_request, "Connection: keep-alive") == nullptr || number_of_received_bytes == 0) {
 				break;
 			}
 		}
-		closesocket(acceptSocket);
+		closesocket(accept_socket);
 		std::cout << "accept socket closed" << std::endl;
 	}
 
-	closesocket(mySocket);
+	closesocket(my_socket);
 	std::cout << "main socket closed" << std::endl;
 	WSACleanup();
 }
